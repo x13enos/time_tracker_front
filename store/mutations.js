372df330a7962f18a364107e1a4bad3ee49ba9f0
Andres Sild <x3enos@gmail.com>
2019-09-04 10:57:21 +0300

    TT-13: added testing env for app

--- store/mutations.js
+++ store/mutations.js
@@ -1,6 +1,6 @@
 export default {
   updateUserData (state, userData) {
-    localStorage.setItem('authToken', userData.token)
+    // localStorage.setItem('authToken', userData.token)
     state.user.name = userData.user.name
     this.$router.replace({ path: '/' })
   }
