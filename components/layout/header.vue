aa0925b77049d2545143b6b9ebf04ae7dcbd7172
Andres Sild <x3enos@gmail.com>
2019-09-28 17:55:56 +0300

    TT-17: added layout for logged users

---
+++ components/layout/header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-app-bar
+      color="primary"
+      :dark="true"
+    >
+      <v-toolbar-title>
+        Time Tracker
+      </v-toolbar-title>
+
+      <div class="flex-grow-1" />
+
+      <v-toolbar-items>
+        <v-btn text>
+          Tasks
+        </v-btn>
+      </v-toolbar-items>
+
+      <template>
+        <v-btn icon>
+          <v-icon>
+            mdi-account-circle
+          </v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>
+            mdi-logout
+          </v-icon>
+        </v-btn>
+      </template>
+    </v-app-bar>
+  </div>
+</template>
