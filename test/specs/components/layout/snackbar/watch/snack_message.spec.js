93ba76a2969ceabf58f855a9b742eb23ab91aab4
Andres Sild <X3enos@gmail.com>
2021-09-06 10:03:45 +0300

    Changed way of handling close fucntion for the snackbar (#145)

--- test/specs/components/layout/snackbar/watch/snack_message.spec.js
+++ test/specs/components/layout/snackbar/watch/snack_message.spec.js
@@ -1,11 +1,11 @@
 import createWrapper from '@/test/support/create_wrapper.js';
-import snackbar from '@/components/layout/snackbar';
+import Snackbar from '@/components/layout/snackbar';
 
 describe('watched snack_message', () => {
 
   it('should set show value as true if message is present', async () => {
     const store = fakeStoreData();
-    const wrapper = createWrapper(snackbar, {}, store);
+    const wrapper = createWrapper(Snackbar, {}, store);
     wrapper.vm.show = false;
     store.state.snack.message = "test message";
     await wrapper.vm.$nextTick;
@@ -13,13 +13,9 @@ describe('watched snack_message', () => {
   });
   
   it('should not set show value as true if message is empty', async () => {
-    const store = fakeStoreData();
-    const wrapper = createWrapper(snackbar, {}, store);
-    store.state.snack.message = "test message";
-    wrapper.vm.show = false;
-    store.state.snack.message = "";
-    await wrapper.vm.$nextTick;
-    expect(wrapper.vm.show).to.be.false;
+    const localThis = { show: true };
+    Snackbar.watch["snack.message"].call(localThis, '');
+    expect(localThis.show).to.be.false;
   });
 
 });
